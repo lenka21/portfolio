687b78924f2834a36db6c15cde1f3668614a7769
Krzysztof Szumny <noisy.pl@gmail.com>
2021-08-04 21:57:57 +0000

    deploy

---
+++ docs/js/about.d4afcc40.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),u={class:"about"},c=Object(a["i"])("h1",null,"This is an about page",-1);function o(n,t){return Object(a["u"])(),Object(a["e"])("div",u,[c])}const s={};s.render=o;t["default"]=s}}]);
+//# sourceMappingURL=about.d4afcc40.js.map
